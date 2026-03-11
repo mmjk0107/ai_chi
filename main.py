771b8ec6c483ea534a2c7084dba0c380e2ee3802
iuiu0107 <mmjk0107@gmail.com>
2026-03-11 16:21:37 +0800

    feat: Initialize and invoke `ChatOpenAI` with `gpt-4o-mini` to replace the initial greeting.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import openai.types.responses.response
+from langchain_ollama import ChatOllama
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
 def main():
-    print("Hello from ai-chi!")
+    llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.7)
+    response = llm.invoke("Hello, how are you?")
+    print(response.content)
 
 
 if __name__ == "__main__":
